60bced0fea63999ca2c6f1f262293ced26bd13de
Bekir <l3ekirerdem@gmail.com>
2021-05-26 00:53:05 +0300

    initial

---
+++ project.js
@@ -0,0 +1,34 @@
+const Form = document.getElementById("film-form");
+const titleElement = document.querySelector("#title");
+const directorElement = document.querySelector("#director");
+const urlElement = document.querySelector("#url");
+
+// UI Objesini başlatma
+const ui = new UI();
+
+// Tüm eventleri yükleme
+
+eventListeners();
+
+function eventListeners() {
+  form.addEventListener("submit", addFilm);
+}
+function addFilm(e) {
+  const title = titleElement.value;
+  const director = directorElement.value;
+  const url = urlElement.value;
+
+  if (title === "" || director === "" || url === "") {
+    // Hata mesajı
+  } else {
+    // Yeni film ekleme
+    const newFilm = new Film(title, director, url);
+
+    // Arayüze film ekleme
+    ui.addFilmToUI(newFilm);
+  }
+
+  ui.clearInputs(titleElement,directorElement,urlElement);
+
+  e.preventDefault();
+}
