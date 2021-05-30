56dfcf43bfd88944099b485ffbdd3cb4238ea435
Bekir <l3ekirerdem@gmail.com>
2021-05-30 23:56:41 +0300

    bilgilendirme ve hata mesajları

--- ui.js
+++ ui.js
@@ -2,13 +2,6 @@ function UI() {}
 
 UI.prototype.addFilmToUI = function (newFilm) {
   
-/*     <!-- <tr>
-    <td><img src="" class="img-fluid img-thumbnail"></td>
-    <td></td>
-    <td></td>
-    <td><a href="#" id = "delete-film" class = "btn btn-danger">Filmi Sil</a></td>
-    </tr> --> */
-
     const filmList = document.getElementById('films')
 
     filmList.innerHTML += `
@@ -25,4 +18,19 @@ UI.prototype.clearInputs = function (element1,element2,element3) {
     element1.value = "";
     element2.value = "";
     element3.value = "";
+}
+
+UI.prototype.displayMessages = function (message, type) {
+    const cardBody = document.querySelector(".card-body")
+
+    const div = document.createElement('div')
+
+    div.className= `Alert Alert${type}`
+    div.textContent = message
+
+    cardBody.appendChild(div)
+
+    setTimeout(function () {
+        div.remove()
+    },3000)
 }
